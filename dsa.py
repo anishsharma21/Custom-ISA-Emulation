72cf6cc13a9d0d0fb5b9c9452dd93d027a15371e
anishsharma21 <anisharma2103@gmail.com>
2024-08-04 23:33:10 +1000

    Binary search

--- dsa.py
+++ dsa.py
@@ -1,10 +1,17 @@
 from typing import List
 
 def binary_search(target: int, nums: List[int]=[]) -> int:
-  
+  l, h = 0, len(nums) - 1
+  while l <= h:
+    m = (l + h) // 2
+    if target == nums[m]:
+      return m
+    elif target < nums[m]:
+      h = m - 1
+    else:
+      l = m + 1
   return -1
 
-nums = [4, 2, 1, 3]
-print(nums)
-nums = binary_search(3, nums)
-print(nums)
+nums = [1, 2, 3, 4, 5, 6, 7]
+target = int(input("What number do you want to find? "))
+print(binary_search(target, nums))
