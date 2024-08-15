1d31f7cd8530f17d63879f942e11735f2d966843
anishsharma21 <anisharma2103@gmail.com>
2024-08-16 00:03:32 +1000

    Setting up emulator (h-emulator.py) - input file name and loading that into memory

---
+++ isa/h-emulator.py
@@ -0,0 +1,17 @@
+from typing import TextIO
+
+while True:
+  file_name: str = str(input("Enter the file name: "))
+  
+  if file_name in ["exit", "quit", "q", "x"]:
+    break
+  elif not file_name.endswith == ".txt":
+    file_name += ".txt"
+
+  try:
+    file: TextIO = open(f'programs/{file_name}', 'r')
+    print(file.read())
+    file.close()
+  except FileNotFoundError:
+    print("File not found")
+    print()
