690b20255cecad861b1fe21adcd8d8c67dfea884
anishsharma21 <anisharma2103@gmail.com>
2024-08-16 20:04:51 +1000

    Program reads in file contents, parses the program, and prints the starting memory output once data is loaded in - prints once to the terminal, and works only for raw format currently

--- h-emulator.py
+++ h-emulator.py
@@ -9,9 +9,30 @@ while True:
     file_name += ".txt"
 
   try:
-    file: TextIO = open(f'../programs/{file_name}', 'r')
-    print(file.read())
+    file: TextIO = open(f'./programs/{file_name}', 'r')
+    contents: list[str] = file.readlines()
+    contents = [s.removesuffix('\n') for s in contents if s != "\n" and s != ""]
+    ram = {}
+    meminput = False
+    curAddress = 0x0000
+    for line in contents:
+      if line[-1] == ":":
+        curAddress = int(line.removesuffix("h:"), 16)
+        meminput = True
+        ram[curAddress] = 0x00
+      elif meminput:
+        ram[curAddress] = int(line.removesuffix("h"), 16)
+        curAddress += 1
+
+    prevAddress = 0
+    for address, value in ram.items():
+      if prevAddress < address - 1:
+        print("...")
+      prevAddress = address
+      print(f"{hex(address)}: {hex(value)}")
+
     file.close()
+    break
   except FileNotFoundError:
     print("File not found")
     print()
