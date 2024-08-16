da780cca4bb63ffca96aabc9733b2a412444e062
anishsharma21 <anisharma2103@gmail.com>
2024-08-16 23:50:37 +1000

    4 digit addresses now appearing in memory, mostly just optimising the code, refactoring, errors being caught near where the try is being used, easier to read now too, will probably need to refactor this into abstracted methods later on, but just trying to get it to work and have it generally well written so I can keep iterating

--- h-emulator.py
+++ h-emulator.py
@@ -1,38 +1,41 @@
-from typing import TextIO
-
 while True:
-  file_name: str = str(input("Enter the file name: "))
+  file_name: str = str(input("Enter the file name: ")).strip()
   
   if file_name in ["exit", "quit", "q", "x"]:
     break
-  elif not file_name.endswith == ".txt":
+
+  if not file_name.endswith(".txt"):  
     file_name += ".txt"
 
+  contents: list[str] = []
+
   try:
-    file: TextIO = open(f'./programs/{file_name}', 'r')
-    contents: list[str] = file.readlines()
-    contents = [s.removesuffix('\n') for s in contents if s != "\n" and s != ""]
-    ram = {}
-    meminput = False
-    curAddress = 0x0000
-    for line in contents:
-      if line[-1] == ":":
-        curAddress = int(line.removesuffix("h:"), 16)
-        meminput = True
-        ram[curAddress] = 0x00
-      elif meminput:
-        ram[curAddress] = int(line.removesuffix("h"), 16)
-        curAddress += 1
-
-    prevAddress = 0
-    for address, value in ram.items():
-      if prevAddress < address - 1:
-        print("...")
-      prevAddress = address
-      print(f"{hex(address)}: {hex(value)}")
-
-    file.close()
-    break
+    with open(f'./programs/{file_name}', 'r') as file:
+      contents: list[str] = file.readlines()
   except FileNotFoundError:
     print("File not found")
     print()
+  except Exception as e:
+    print(f"Error: {e}")
+    print()
+
+  contents = [s.strip() for s in contents if s.strip()]
+  
+  ram = {}
+  curAddress = 0x0000
+  for line in contents:
+    if line[-1] == ":":
+      curAddress = int(line.removesuffix("h:"), 16)
+      ram[curAddress] = 0x00
+    elif curAddress in ram:
+      curAddress += 1
+      ram[curAddress] = int(line.removesuffix("h"), 16)
+
+  prevAddress = 0
+  for address, value in ram.items():
+    if prevAddress < address - 1:
+      print("...")
+    prevAddress = address
+    print(f"{address:#06x}: {value:#06x}")
+
+  break
