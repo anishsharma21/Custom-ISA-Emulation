d90fe5c991649d0eb4b6c3e9a38452534ade7c0e
anishsharma21 <anisharma2103@gmail.com>
2024-08-18 17:04:45 +1000

    Fixed JZ operation! Program operates as intended, you can step forward through the program as it executes, you can see the current operation being executed with a description, you can see the memory map changing, instruction number is displayed, title for final memory is also displayed, clean execution of the subtraction loop, colouring using ANSI escape codes too

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from typing import List
 from helpers import get_file_contents, get_ram_from_mem_contents, find_operation_from_opcode, render_mem_map_from_ram
 from exceptions import InvalidOpcode, InvalidAddress
 
+# ANSI escape codes for background colors
+BG_YELLOW = "\033[43m"
+BG_BLUE = "\033[44m"
+BG_ORANGE = "\033[48;5;202m"
+RESET = "\033[0m"
+
 def main():
   file_name: str = str(input("Enter the file name: ")).strip()
   
@@ -11,12 +17,17 @@ def main():
 
   file_contents: List[str] = get_file_contents(file_name)
   ram: dict[int, List[int]] = get_ram_from_mem_contents(file_contents)
+  index: int = 0
+  
   os.system('clear')
+  print(f"\033[38;5;27mInstruction 0\033[0m")
+  print()
+  print("-" * 10)
   render_mem_map_from_ram(ram)
+  print("-" * 10)
 
   accumulator: int = 0x0000
   
-  index: int = 0
   while index < len(file_contents):
     line: str = file_contents[index]
     if line[-1] == ":" or line[0] == ";": # skip memory address declaration or comments
@@ -31,10 +42,6 @@ def main():
     index += 3
 
     print()
-
-    # ANSI escape codes for background colors
-    BG_YELLOW = "\033[43m"
-    RESET = "\033[0m"
 
     print(f"Accumulator value: {accumulator:#04x}")
     
@@ -57,33 +64,37 @@ def main():
       # not implemented
       pass
     elif operation == "JZ":
-      jumped: bool = False
-      for j in range(len(file_contents) - 1):
-        print(file_contents[j], file_contents[j+1])
-        print(f"{address:#06x}")
-        print((f"{address:#06x}")[2:4] + "h")
-        print((f"{address:#06x}")[4:6] + "h")
-        if file_contents[j] == ((f"{address:#06x}")[2:4] + "h") and file_contents[j+1] == ((f"{address:#06x}")[4:6] + "h") or file_contents[j] == ((f"{address:#06x}")[4:6] + "h") == "ffh":
-          if accumulator == 0x00:
-            i = j
-            print(f"Not jumping since accumulator is 0x00")
-          else:
-            print(f"Jumping to address {address}")
-          jumped = True
-          break
-      if not jumped:
-        raise InvalidAddress(f"Address {address:#06x} was not found\nError on operation {line}")
+      try:
+        jmp_index: int = ram[address][1]
+      except IndexError:
+        raise InvalidAddress(f"Value {ram[address][0]} is not declared in original file and cannot be jumped to\nError at address {address}\nError on operation {line}")
+      
+      if accumulator == 0x00:
+        print(f"{BG_ORANGE}Not jumping since accumulator is 0x00{RESET}")
+      else:
+        index = jmp_index
+        print(f"{BG_ORANGE}Jumping to address {int(file_contents[jmp_index].removesuffix("h:"), 16):#06x}{RESET}")
+
     else:
         raise InvalidOpcode(f"{line} is an invalid opcode")
     
     print()
     input("Press enter to see updated ram")
     os.system('clear')
+    print(f"\033[38;5;27mInstruction {(index - 1) // 3 if index > 0 else 0}\033[0m")
+    print()
+    print("-" * 10)
     render_mem_map_from_ram(ram)
+    print("-" * 10)
   
   # final ram memory map
   os.system('clear')
+  print(f"\033[38;5;27mFinal Memory Map of RAM\033[0m")
+  print()
+  print("-" * 10)
   render_mem_map_from_ram(ram)
+  print("-" * 10)
+  print()
 
 if __name__ == "__main__":
   main()
